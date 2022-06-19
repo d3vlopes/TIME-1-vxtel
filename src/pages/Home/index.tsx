3cf93e1445cb21a2ae461090d0cd20ea8b4dc1f7
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-06-19 16:52:50 -0300

    WIP

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,23 @@
-import { SampleComponent } from 'components/SampleComponent'
+import { Table } from 'components/Table'
+import { calculatePrice } from 'services/calculatePrice'
+
+import { formatPrice } from 'utils/formatPrice'
 
 export const Home = () => {
-  return <SampleComponent title="Hello World Union group" />
+  const { finalValueWithPlan, finalValueWithoutPlan } = calculatePrice(
+    '016',
+    '011',
+    40,
+    30,
+  )
+  const dataBody = [
+    '016',
+    '011',
+    '30',
+    'FaleMais30',
+    formatPrice(finalValueWithPlan),
+    formatPrice(finalValueWithoutPlan),
+  ]
+
+  return <Table dataBody={dataBody} />
 }
